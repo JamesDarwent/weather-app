3d6dd6f8460e152513a5437c96b84362499e45d4
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-04-20 16:21:04 +0100

    LocationDetails component completed. PropTypes validation added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import '../styles/App.css';
-import React from 'react';
+import "../styles/App.css";
+import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
-  return (
-    <div className="prop">
-     <h1>Weather App</h1>
-    </div>
-  );
-}
+// const App = (props) => {
+//   return (
+//     <div className="App">
+//       <LocationDetails city={props.location.city} country={props.location.country} />
+//     </div>
+//   );
+// };
+
+// Below is a deconstructed version of the above component
+
+const App = ({ location }) => {
+  const { city, country } = location;
+  return <LocationDetails city={city} country={country} />;
+};
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import { location } from './data/forecast.json';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import { location } from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App location = { location } />
+    <App location={location} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
